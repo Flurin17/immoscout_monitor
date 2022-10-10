c504ad48a3eaa6451dfa81a76a7ee1ab59f8c07a
Flurin <53998860+Flurin17@users.noreply.github.com>
2022-10-10 19:12:10 +0200

    added contact infos

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 immoscoutLink = "https://rest-api.immoscout24.ch/v4/de/properties?af=2306&nrf=3.5&pt=22h&r=40&s=1&se=16&t=1"
 mongoDBUrl = ""
 discordWebhook = ""
-delay = 10
+delay = 120
+
+sendContactRequest = False
+fullname= "Max Muster"
+email = "test.test@gmail.com"
+phoneNr = "+41857234234"
