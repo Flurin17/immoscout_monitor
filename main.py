68545a62f93a9a992753c50ee89702c26bf38df6
Flurin <flurin.laim@gmail.com>
2022-10-10 19:11:10 +0200

    added sending contact request

--- main.py
+++ main.py
@@ -54,66 +54,113 @@ class Monitor:
         response = webhook.execute()
     
     def get_page(self):
-        r = requests.get(
-            url=immoscoutLink,
-            headers={
-                "Accept": "text/plain, application/json, text/json",
-                "Accept-Encoding": "gzip, deflate, br",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Connection": "keep-alive",
-                "Host": "rest-api.immoscout24.ch",
-                "is24-meta-pagenumber": "1",
-                "Origin": "https://www.immoscout24.ch",
-                "Referer": "https://www.immoscout24.ch/",
-                "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-                "X-OriginalUrl": "http%3A%2F%2Fwww.immoscout24.ch%2Fde"
+        while True:
+            r = requests.get(
+                url=immoscoutLink,
+                headers={
+                    "Accept": "text/plain, application/json, text/json",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Accept-Language": "en-US,en;q=0.9",
+                    "Connection": "keep-alive",
+                    "Host": "rest-api.immoscout24.ch",
+                    "is24-meta-pagenumber": "1",
+                    "Origin": "https://www.immoscout24.ch",
+                    "Referer": "https://www.immoscout24.ch/",
+                    "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+                    "X-OriginalUrl": "http%3A%2F%2Fwww.immoscout24.ch%2Fde"
+                }
+            )
+
+            try:
+                if r.status_code == 200:
+                    responseJson = r.json()
+                    properties = responseJson["properties"]
+                    try:
+                        for i in properties:
+                            id = i["id"]
+                            cityName = i["cityName"]
+                            normalizedPriceFormatted = i["normalizedPriceFormatted"]
+                            numberOfRoomsFormatted = i["numberOfRoomsFormatted"]
+                            try:
+                                surfaceLivingFormatted = i["surfaceLivingFormatted"]
+                            except:
+                                surfaceLivingFormatted = "Unknown"
+                            propertyUrl = "https://www.immoscout24.ch" + i["propertyUrl"]
+
+                            try:
+                                imageData = i["images"][0]
+                                imageUrl = imageData["url"].replace('{width}', str(imageData["originalWidth"])).replace('{height}', str(imageData["originalHeight"])).replace('{resizemode}', "1").replace('{quality}', "100000")
+                            except Exception as e:
+
+                                imageUrl = "https://jfv-asp.de/wp-content/themes/ryse/assets/images/no-image/No-Image-Found-400x264.png"
+                            latitude = i["latitude"]
+                            longitude = i["longitude"]
+
+                            propertie = propertieClass(
+                                id,
+                                cityName,
+                                normalizedPriceFormatted,
+                                numberOfRoomsFormatted,
+                                surfaceLivingFormatted,
+                                propertyUrl,
+                                imageUrl,
+                                latitude,
+                                longitude,
+
+                            )
+                            self.propertiesList.append(asdict(propertie))
+                            break
+
+                    except Exception as e:
+                        logging.error(f"Failed to capture propertie. Error : {e}")
+            except:
+                logging.error(f"Status code: {r.status_code}")
+                
+            time.sleep(20)
+
+    def send_message(self, id):
+        r = requests.post(
+            url=f'https://rest-api.immoscout24.ch/v4/de/properties/{str(id)}/contacts',
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:105.0) Gecko/20100101 Firefox/105.0',
+                'Accept': 'text/plain, application/json, text/json',
+                'Accept-Language': 'en-GB,en;q=0.5',
+                # 'Accept-Encoding': 'gzip, deflate, br',
+                'Referer': 'https://www.immoscout24.ch/',
+                # Already added when you pass json=
+                # 'Content-Type': 'application/json',
+                'X-OriginalClientIp': '212.243.177.218',
+                'X-OriginalUrl': 'http%3A%2F%2Fwww.immoscout24.ch%2Fde',
+                'Origin': 'https://www.immoscout24.ch',
+                'DNT': '1',
+                'Connection': 'keep-alive',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-site',
+                'Pragma': 'no-cache',
+                'Cache-Control': 'no-cache',
+            },
+            json = {
+                'message': 'Guten Tag\nIch möchte einen Besichtigungstermin vereinbaren.\n Bitte kontaktieren Sie mich.',
+                'fullName': fullname,
+                'email': email,
+                'phone': phoneNr,
+                'propertyId': int(id),
+                'contactFormTypeId': 2,
+                'searchGroupId': 1,
+                'searchOfferTypeId': 1,
+                'searchLocationIds': '4147',
+                'searchRadius': '40',
             }
         )
 
         try:
             if r.status_code == 200:
-                responseJson = r.json()
-                properties = responseJson["properties"]
-                try:
-                    for i in properties:
-                        id = i["id"]
-                        cityName = i["cityName"]
-                        normalizedPriceFormatted = i["normalizedPriceFormatted"]
-                        numberOfRoomsFormatted = i["numberOfRoomsFormatted"]
-                        try:
-                            surfaceLivingFormatted = i["surfaceLivingFormatted"]
-                        except:
-                            surfaceLivingFormatted = "Unknown"
-                        propertyUrl = "https://www.immoscout24.ch" + i["propertyUrl"]
-
-                        try:
-                            imageData = i["images"][0]
-                            imageUrl = imageData["url"].replace('{width}', str(imageData["originalWidth"])).replace('{height}', str(imageData["originalHeight"])).replace('{resizemode}', "1").replace('{quality}', "100000")
-                        except Exception as e:
-
-                            imageUrl = "https://jfv-asp.de/wp-content/themes/ryse/assets/images/no-image/No-Image-Found-400x264.png"
-                        latitude = i["latitude"]
-                        longitude = i["longitude"]
-
-                        propertie = propertieClass(
-                            id,
-                            cityName,
-                            normalizedPriceFormatted,
-                            numberOfRoomsFormatted,
-                            surfaceLivingFormatted,
-                            propertyUrl,
-                            imageUrl,
-                            latitude,
-                            longitude,
-
-                        )
-                        self.propertiesList.append(asdict(propertie))
-
-                except Exception as e:
-                    logging.error(f"Failed to capture propertie. Error : {e}")
+               logging.info("Sent contact request")
         except:
             logging.error(f"Status code: {r.status_code}")
+            
 
     def check_new_properties(self):
         foundNew = False
@@ -124,8 +171,9 @@ class Monitor:
             else:
                 logging.info(f"Found new propertie in {propertie['location']}")
                 self.send_webhook(propertie)
-                logging.info("Send webhook")
                 db.insert_one(propertie)
+                if sendContactRequest:
+                    self.send_message(propertie['id'])
                 foundNew = True
         
         if foundNew == False:
